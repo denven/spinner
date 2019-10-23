d262ad4807faa70882444cfa599a718dc6e0eb0f
denven <myregs@foxmail.com>
2019-10-22 20:11:20 -0700

    change var to let

--- spinner2.js
+++ spinner2.js
@@ -1,5 +1,5 @@
 const chars = ["|", "/", "—", "\\"];
 
-for (var i = 0, j = 0; i < 10 * chars.length; i++) { //print 10 cycles
-  setTimeout(() => { process.stdout.write("\r" + chars[j++ % 4]);}, 100 * (i + 1));
+for (let i = 0; i < 10 * chars.length; i++) { //print 10 cycles
+  setTimeout(() => { process.stdout.write("\r" + chars[i % 4]);}, 100 * (i + 1));
 }
